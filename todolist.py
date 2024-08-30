fb0ec1334d8728f89d1a95c956f9dc83d7a7b2c3
SValenziano <senorvalenz@gmail.com>
2024-08-30 11:47:44 -0400

    Step 7 complete

--- todolist.py
+++ todolist.py
@@ -53,6 +53,26 @@ class TodoList:
         output_lines += [str(todo) for todo in self._todos]
         return '\n'.join(output_lines)
 
+    def __len__(self):
+        return len(self._todos)
+    
+    def first(self):
+        return self._todos[0]
+
+    def last(self):
+        return self._todos[-1]
+    
+    def to_list(self):
+        return self._todos.copy()
+    
+    def todo_at(self, index:int):
+        return self._todos[index]
+    
+    def mark_done_at(self, index:int):
+        self.todo_at(index).done = True
+
+    def mark_undone_at(self, index:int):
+        self.todo_at(index).done = False
 
 
 # TESTS ***********************************************************
@@ -101,4 +121,125 @@ def step_2():
     # [X] Clean room
     # [ ] Go to gym
 
-step_2()
+# step_2()
+
+
+def step_3():
+    print('--------------------------------- Step 3')
+    todo_list = setup()
+
+    print(len(todo_list))              # 3
+    print(len(empty_todo_list))        # 0
+
+# step_3()
+
+def step_4():
+    print('--------------------------------- Step 4')
+    todo_list = setup()
+
+    print(todo_list.first())           # [ ] Buy milk
+    print(todo_list.last())            # [ ] Go to gym
+
+    try:
+        empty_todo_list.first()
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    try:
+        empty_todo_list.last()
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+# step_4()
+
+def step_5():
+    print('--------------------------------- Step 5')
+    todo_list = setup()
+
+    print(empty_todo_list.to_list())    # []
+
+    todos = todo_list.to_list()
+    print(type(todos).__name__)         # list
+
+    for todo in todos:
+        print(todo)                     # [ ] Buy milk
+                                        # [X] Clean room
+                                        # [ ] Go to gym
+
+# step_5()
+
+def step_6():
+    print('--------------------------------- Step 6')
+    todo_list = setup()
+
+    print(todo_list.todo_at(0))        # [ ] Buy milk
+    print(todo_list.todo_at(1))        # [X] Clean room
+    print(todo_list.todo_at(2))        # [ ] Go to gym
+
+    try:
+        todo_list.todo_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    # Ensure we have a reference
+    print(todo_list.todo_at(1) is todo_list.todo_at(1))  # True
+
+# step_6()
+
+def step_7():
+    print('--------------------------------- Step 7')
+    todo_list = setup()
+
+    todo_list.mark_done_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room'
+    # [ ] Go to gym
+
+    todo_list.mark_done_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room'
+    # [ ] Go to gym
+
+    todo_list.mark_done_at(2)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room'
+    # [X] Go to gym
+
+    try:
+        todo_list.mark_done_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    todo_list.mark_undone_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room'
+    # [X] Go to gym
+
+    todo_list.mark_undone_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room'
+    # [X] Go to gym
+
+    todo_list.mark_undone_at(2)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room'
+    # [ ] Go to gym
+
+    try:
+        todo_list.mark_undone_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+step_7()
